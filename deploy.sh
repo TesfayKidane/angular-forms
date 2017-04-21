b7e6f38298b28347a394fa93bfb08d114e8473de
Tesfay Aregay <Tesfay Aregay>
2017-04-20 20:25:26 -0500

    form update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ng build --prod --base-href="/angular4-forms"
+ng build --prod --base-href="/angular4-forms/"
 git add .
 git commit -m "form update"
 git subtree push --prefix dist origin gh-pages
